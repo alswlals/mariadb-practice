3c7d437a68fd3dc08b714331b852d7e48363073b
alswlals <awlals616@gmail.com>
2023-01-11 13:50:21 +0900

    JDBC #1

---
+++ jdbc-practice/src/main/java/test/ConnectionTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			/* 1. JDBC Driver Class Loading*/
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			/* 2. 연결하기*/
+			String url = "jdbc:mariadb://192.168.10.114:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			System.out.println("연결 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver Loading error:" + e);
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+}
